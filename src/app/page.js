30a4c40b72066a54773e9c5d12911cd4b2e724d5
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-07-25 02:34:38 -0700

    creating the swiper page

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import NavigationBar from "@/components/NavigationBar";
 import Simple from "@/components/Simple";
+import Swiper from "@/components/Swiper";
 
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <NavigationBar/>
       <Hero/>
       <About/>
+      <Swiper/>
       <Simple/>
       <Footer/>
     </>
